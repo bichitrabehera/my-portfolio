8c9ed229e5906f4ceeedc536c44916a23fe7579c
Bichitra Behera <bichitrabehera.amcec@gmail.com>
2025-03-22 20:29:55 +0530

    Update Skills.jsx

--- src/Components/sections/Skills.jsx
+++ src/Components/sections/Skills.jsx
@@ -4,7 +4,7 @@ function Skills() {
   return (
     <section id="skills" className="py-20">
         <h2 className="text-3xl font-bold text-[#06B6D4]">What I Use</h2>
-        <div className="w-20 h-1 bg-purple-700  mt-2 mb-8 rounded"></div>
+        <div className="w-20 h-1 bg-orange-600  mt-2 mb-8 rounded"></div>
       <div className="skills-vert py-8">
         
 
